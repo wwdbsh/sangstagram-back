088ea0a817d1bb5a03e7ea7bacbf058b5b6cc9fc
Sangheon Lee <wwdbsh@gmail.com>
2020-06-06 14:13:43 +0900

    4th update

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -1,9 +1,9 @@
-import { generateSecret, sendSecretMail } from "../../../utils";
 import { prisma } from "../../../../generated/prisma-client";
+import { generateSecret, sendSecretMail } from "../../../utils";
 
 export default{
     Mutation:{
-        requestSecret: async (_, args) => {
+        requestSecret: async (_, args, {request}) => {
             const { email } = args;
             const loginSecret = generateSecret();
             console.log(loginSecret);
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({path:path.resolve(__dirname, ".env")});
-
 import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import mg from "nodemailer-mailgun-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -37,3 +34,5 @@ export const sendSecretMail = (address, secret) => {
     };
     return sendMail(email);
 };
+
+export const generateToken = id => jwt.sign({id}, process.env.JWT_SECRET);
