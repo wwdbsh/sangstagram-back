009c849ec991171a1b11e5917c3e598b6def932d
Sangheon Lee <wwdbsh@gmail.com>
2020-12-16 17:49:21 -0500

    20201216

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -24,6 +24,5 @@ export default{
             });
         },
         likeCount: parent => prisma.likesConnection({where:{post:{id:parent.id}}}).aggregate().count(),
-        // commentCount: parent => prisma.commentsConnection({where:{post:{id:parent.id}}}).aggregate().count()
     }
 };
